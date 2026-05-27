09417073b1d45f796e9790b797b64c654908b928
Namkha Gyatso <namkhagyatso999@gmail.com>
2026-05-27 15:52:50 +0545

    fix rewrite destination to use env var

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3001/:path*",
+        destination: `${process.env.BACKEND_URL_SERVER || "http://localhost:3001"}/:path*`,
       },
     ];
   },
