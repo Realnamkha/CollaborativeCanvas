67c21b433814932349057c0a1498d80f19d0aa8b
Namkha Gyatso <namkhagyatso999@gmail.com>
2026-05-27 15:49:20 +0545

    fix BACKEND_URL to use /api proxy

--- apps/web/config.ts
+++ apps/web/config.ts
@@ -1,5 +1,4 @@
 export const BACKEND_URL_SERVER =
   process.env.BACKEND_URL_SERVER || "http://localhost:3001";
-export const BACKEND_URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3001";
+export const BACKEND_URL = "/api";
 export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8080";
